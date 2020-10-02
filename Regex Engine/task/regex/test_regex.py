2f3a176c9aabb0edff4461d38e0261397ef87cbd
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-10-02 15:34:55 +0300

    stage 4/6

---
+++ Regex Engine/task/regex/test_regex.py
@@ -0,0 +1,22 @@
+import unittest
+import regex
+
+
+class Test(unittest.TestCase):
+    def test_match(self):
+        self.assertTrue(regex.match('^app', 'apple'))
+        self.assertTrue(regex.match('^a', 'apple'))
+        self.assertTrue(regex.match('^apple', 'apple pie'))
+        # self.assertTrue(regex.match('le$', 'apple'))
+        # self.assertTrue(regex.match('.$', 'apple'))
+        # self.assertTrue(regex.match('apple$', 'tasty apple'))
+        # self.assertTrue(regex.match('^apple$', 'apple'))
+        # self.assertFalse(regex.match('^le', 'apple'))
+        # self.assertFalse(regex.match('^apple$', 'tasty apple'))
+        # self.assertFalse(regex.match('^apple$', 'apple pie'))
+        # self.assertFalse(regex.match('^app$', 'apple'))
+        # self.assertFalse(regex.match('app$', 'apple'))
+
+
+if __name__ == '__main__':
+    unittest.main()
